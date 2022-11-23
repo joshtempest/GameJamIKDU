30ec668c384e8477bf29a8cbfd55a3535198b191
Adrenalina333 <115779476+Adrenalina333@users.noreply.github.com>
2022-11-23 16:06:31 +0100

    MoneyCounter

---
+++ GameJam/Assets/Scripts/MoneyCounter.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using System;
+
+public class MoneyCounter : MonoBehaviour
+{
+
+    public static MoneyCounter instance;
+    private CostManager costManager;
+    private TimeManager timeManager;
+
+    public TMP_Text moneyText;
+    private float currentMoney = 0f;
+    private float price;
+
+    // Det er kunderne der skal have den her
+    public bool OrderRecieved = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        costManager = GameObject.find("CostManager").GetComponent<CostManager>();
+        timeManager = GameObject.find("TimeManager").GetComponent<TimeManager>();
+        moneyText.text = "MONEY: " + currentMoney.ToString();
+    }
+
+
+    void FixedUpdate()
+    {
+        // MIGHT NOT BE NAMED GET NIGHT!!!!
+        if(timeManager.GetNight()){
+
+            //subtract final price
+            //Reset finalprce from time manager OR somehow make sure this only subtracts once per night.
+            //currentMoney -= finalprice;
+            //return currentMoney;
+
+
+
+        }
+
+    }
+
+
+    /*
+    public void addMoney()
+    {
+        currentMoney += 5;
+    }
+
+    public void takeMoney()
+    {
+        currentMoney -= price;
+    }
+
+    public float getPrice(){
+        return price;
+    }
+
+    public void setPrice(value)
+    */
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- GameJam/Assets/Scripts/UpdradeManager.cs
+++ GameJam/Assets/Scripts/UpdradeManager.cs
@@ -1,9 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using System;
 
 public class UpdradeManager : MonoBehaviour
 {
+
+    //public gameObject OrderPickup;
+    public int currentMoney;
+    public string labelText = "Money";
+    public TMP_Text moneyText;
+    public int value;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +27,33 @@ public class UpdradeManager : MonoBehaviour
     {
         
     }
+
+    void FixedUpdate ()
+    {
+        //if (OrderPickup = (true))
+        {
+            //currentMoney =+ 1;
+            //Debug.Log("You now have" + currentMoney);
+        }
+           
+    }
+
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        if(OrderPickup != null)
+        {
+            currentMoney +1;
+            MoneyCounter.instance.increaseMmoney(value);
+        }
+
+    }
+
+    void OnGUI()
+    {
+        GUI.Box (new Rect(20, 20, 150, 25), "Money" + currentMoney);
+        }
 }
+
+
+      
+
