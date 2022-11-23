9f47b5f16c8600dbf6fc3f90b43dafe6643ab9b3
joshtempest <joshuabussink29@gmail.com>
2022-11-23 13:20:09 +0100

    Update CostManager.cs
    fixed some bits

--- GameJam/Assets/Scripts/CostManager.cs
+++ GameJam/Assets/Scripts/CostManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CostManager : MonoBehaviour
 {
+    //Declares all the variables needed
     public float elecPrice = 1f;
     public float onionPrice = 1f;
     public float tomatoPrice = 1.5f;
@@ -12,17 +13,27 @@ public class CostManager : MonoBehaviour
     public float finalPrice;
     public float allIngrePrice;
 
-    //magic numbers
+    //Magic numbers, stand-ins from other scripts
     public int day = 3;
     public float Time = 3;
+    public int tomatoUsed = 2;
+    public int carrotUsed = 1;
+    public int onionUsed = 1;
+    public int saltUsed = 0;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        //Calculates the total electricity price for the day
         elecPrice = elecPrice + ((float)day / 2);
         Debug.LogFormat("Elecprice is: {0}",elecPrice);
-        allIngrePrice = (2 * tomatoPrice) + carrotPrice + onionPrice;
+
+        //Calculates the price of all ingredients used
+        allIngrePrice = ((float)tomatoUsed * tomatoPrice) + ((float)carrotUsed * carrotPrice) + ((float)onionUsed * onionPrice) + ((float)saltUsed * saltPrice);
         Debug.Log(allIngrePrice);
+
+        //Calculates the final price for the day
         finalPrice = (elecPrice * Time) + allIngrePrice;
         Debug.LogFormat("final price for today is {0}", finalPrice);
     }
