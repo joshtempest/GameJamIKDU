6b202092c671676558366d0896e98c234de6e528
Adrenalina333 <115779476+Adrenalina333@users.noreply.github.com>
2022-11-24 14:45:18 +0100

    H

--- GameJam/Assets/Scripts/MoneyCounter.cs
+++ GameJam/Assets/Scripts/MoneyCounter.cs
@@ -8,29 +8,17 @@ using System;
 public class MoneyCounter : MonoBehaviour
 {
 
-    public static MoneyCounter instance;
-    //private CostManager costManager;
-    //private TimeManager timeManager;
     public GameObject CostController;
     public GameObject TimeController;
 
-    public TMP_Text moneyText;
     private float currentMoney = 3f;
-    private float price;
     private bool nightTime;
     private float lastPrice;
 
-    // Det er kunderne der skal have den her
-    //public bool OrderRecieved = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        //costManager = CostController.GetComponent<CostManager>().finalPrice;
-        //timeManager = GameObject.Find("TimeManager").GetComponent<TimeManager>();
-
-        
-        //moneyText.text = "MONEY: " + currentMoney.ToString();
 
     }
 
@@ -38,42 +26,16 @@ public class MoneyCounter : MonoBehaviour
     {
         lastPrice = CostController.GetComponent<CostManager>().finalPrice;
         nightTime = TimeController.GetComponent<TimeManager>().isNight;
-        // MIGHT NOT BE NAMED GET NIGHT!!!!
+    
         if(nightTime == true)
         {
-            //subtract final price
-            //Reset finalprce from time manager OR somehow make sure this only subtracts once per night.
             currentMoney -= lastPrice;
             lastPrice = 0;
         }
         else
         {
-            currentMoney = currentMoney;
+            
         }
-
-    /*
-    public void addMoney()
-    {
-        currentMoney += 5;
-    }
-
-    public void takeMoney()
-    {
-        currentMoney -= price;
-    }
-
-    public float getPrice(){
-        return price;
-    }
-
-    public void setPrice(value)
-    */
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     }
 
--- GameJam/Assets/Scripts/UpdradeManager.cs
+++ GameJam/Assets/Scripts/UpdradeManager.cs
@@ -3,39 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using UnityEngine.UI;
 using System;
 
 public class UpdradeManager : MonoBehaviour
 {
 
     public int pUpgrade;
-    public GameObject CPButton;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-    
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    void FixedUpdate ()
-    {
-        
-    }
-
+//This function is inwoked by pressing the button
     public void CustomerPatience()
 
     {
-        //OnClicked.GameObject.Find(CPButton);
 
         pUpgrade +2;
 
-        Debug.Log("Hello!");
+    }
+
+    public void Tips()
+
+    {
+        
     }
 
 
