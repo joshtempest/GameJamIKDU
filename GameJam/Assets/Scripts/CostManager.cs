715fcd2370ada010984a04893e9781be041dde84
joshtempest <joshuabussink29@gmail.com>
2022-11-24 13:58:41 +0100

    changed some stuff

--- GameJam/Assets/Scripts/CostManager.cs
+++ GameJam/Assets/Scripts/CostManager.cs
@@ -12,13 +12,14 @@ public class CostManager : MonoBehaviour
     private float carrotPrice = 1.2f;
     public float finalPrice;
     public float allIngrePrice;
+    public int day;
+    public float finalElec;
 
     //Gets all scripts needed
     public GameObject TimeController;
     public TimeManager timeManager;
 
     //Magic numbers, stand-ins from other scripts
-    public int day;
     public float Time = 3;
     public int tomatoUsed = 2;
     public int carrotUsed = 1;
@@ -29,15 +30,18 @@ public class CostManager : MonoBehaviour
    
     void Start()
     {
+        //Calls method to for trial
         PriceCalculation();
     }
 
     public void PriceCalculation()
     {
+        //Gets variable days from timeManager script
         timeManager = TimeController.GetComponent<TimeManager>();
         day = TimeManager.days;
+
         //Calculates the total electricity price for the day
-        elecPrice = elecPrice + ((float)day / 2);
+        finalElec = elecPrice + ((float)day / 2);
         Debug.LogFormat("Elecprice is: {0}", elecPrice);
 
         //Calculates the price of all ingredients used
@@ -45,7 +49,7 @@ public class CostManager : MonoBehaviour
         Debug.Log(allIngrePrice);
 
         //Calculates the final price for the day
-        finalPrice = (elecPrice * Time) + allIngrePrice;
+        finalPrice = (finalElec * Time) + allIngrePrice;
         Debug.LogFormat("final price for today is {0}", finalPrice);
     }
 }
