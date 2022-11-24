8551ffdef6e9d8305ae893815bd0346d7b2ef8cd
Frederik <frederiktaekwondo@outlook.com>
2022-11-24 10:12:34 +0100

    Update CustomerManager.cs

--- GameJam/Assets/Scripts/CustomerManager.cs
+++ GameJam/Assets/Scripts/CustomerManager.cs
@@ -1,18 +1,48 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
+using static System.Net.Mime.MediaTypeNames;
 
 public class CustomerManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+    public float timeRemaining = 10;
+    public bool timerIsRunning = false;
+    public Text timeText;
+    private void Start()
+    {
+        // Starts the timer automatically
+        timerIsRunning = true;
+
     }
+}
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+            timeRemaining -= Time.deltaTime;
+            DisplayTime(timeRemaining);
+            }
+            else
+            {
+            Debug.Log("Time has run out!");
+            timeRemaining = 0;
+            timerIsRunning = false;
+            }
+        }
     }
-}
+    void DisplayTime(float timeToDisplay)
+    {
+    TimeSpan remaining = TimeSpan.FromSeconds(timeToDisplay);
+    timeText.text = remaining.ToString(@”hh\:mm\:ss”);
+    }
+    }
