2da0bcc5c9289c4e272213ff24dc35594d1d038e
HaXr <haxr@outlook.dk>
2022-11-24 10:43:37 +0100

    Fixed CustomerManager

--- GameJam/Assets/Scripts/CustomerManager.cs
+++ GameJam/Assets/Scripts/CustomerManager.cs
@@ -5,22 +5,21 @@ using System.Diagnostics;
 using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 using static System.Net.Mime.MediaTypeNames;
+using TMPro;
 
 public class CustomerManager : MonoBehaviour
 {
+    public float timeRemaining = 10;
+    public bool timerIsRunning = false;
+    public TextMeshProUGUI timeText;
+
     // Start is called before the first frame update
     void Start()
     {
-    public float timeRemaining = 10;
-    public bool timerIsRunning = false;
-    public Text timeText;
-    private void Start()
-    {
         // Starts the timer automatically
         timerIsRunning = true;
-
-    }
-}
+    }    
+ 
 
     // Update is called once per frame
     void Update()
@@ -29,20 +28,22 @@ public class CustomerManager : MonoBehaviour
         {
             if (timeRemaining > 0)
             {
-            timeRemaining -= Time.deltaTime;
-            DisplayTime(timeRemaining);
+                timeRemaining -= Time.deltaTime;
+                SetTimeText(timeRemaining);
             }
+            
             else
             {
-            Debug.Log("Time has run out!");
-            timeRemaining = 0;
-            timerIsRunning = false;
+                //Debug.Log("Time has run out!");
+                print("Time has run out !");
+                timeRemaining = 0;
+                timerIsRunning = false;
             }
         }
     }
-    void DisplayTime(float timeToDisplay)
+
+    public void SetTimeText(float time)
     {
-    TimeSpan remaining = TimeSpan.FromSeconds(timeToDisplay);
-    timeText.text = remaining.ToString(@”hh\:mm\:ss”);
+        timeText.text = "Time Remaining: " + time.ToString();
     }
-    }
+}
